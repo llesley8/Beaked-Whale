03fae252122cc15c24ab60af578b4508cbb58e10
Emma Tashi <etashi@ucsc.edu>
2024-09-03 18:12:47 -0700

    Update bwcutadapt.sh

--- bwcutadapt.sh
+++ bwcutadapt.sh
@@ -9,4 +9,4 @@
 #SBATCH --cpus-per-task=2 # 2 CPUS per job
 #SBATCH --mem=4GB # memory limit of 4 GB
 
-cutadapt -a "A{10}N{90}" -o /hb/groups/kelley_training/Emma/bwfastqc_results/bwtrimmedpolyatailm84053_240607_205216_s1.hifi_reads.bc2072.trimmedpoly.fastq.gz /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpressm84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz
+cutadapt -a "A{10}N{90}" -o /hb/groups/kelley_training/Emma/bwfastqc_results/bwtrimmedpolyatail/m84053_240607_205216_s1.hifi_reads.bc2072.trimmedpoly.fastq.gz /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpress/m84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz
