52597cf2123c29b8962d14fa783d2a176eb64447
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-06 14:29:22 -0700

    Update bwjellyhisto.sh

--- bwjellyhisto.sh
+++ bwjellyhisto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=bwjellyhisto # Name the job bwjellyfish to the queue 
+#SBATCH --job-name=bwjellyhisto # Name the job bwjellyfish to the queue
 #SBATCH --time=0-2:00:00 # Wall clock time limit in Days-Hours:min:seconds
 #SBATCH --mail-user=llugardo@ucsc.edu # send updates to my email
 #SBATCH --mail-type=ALL # send all types of updates
@@ -7,9 +7,9 @@
 #SBATCH --error=bwjellyhisto.err # error file
 #SBATCH --ntasks=1 # Run 1 job
 #SBATCH --ntasks-per-node=1 # one task per computer
-#SBATCH --cpus-per-task=2 # 2 CPUS per job
+#SBATCH --cpus-per-task=8 # 8 CPUS per job
 #SBATCH --mem=4GB # memory limit of 4 GB
 
 module load jellyfish
 
-jellyfish histo -t 10 reads.jf > reads.histo
+jellyfish histo -t 8 reads.jf > reads.histo
