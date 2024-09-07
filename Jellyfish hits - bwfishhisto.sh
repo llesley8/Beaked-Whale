bca809ea28eb9f0241654e0c531cf2e8db9519f1
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-06 21:53:36 -0700

    Update and rename bwjellyhisto.sh to Jellyfish hits - bwfishhisto.sh

--- Jellyfish hits - bwfishhisto.sh
+++ Jellyfish hits - bwfishhisto.sh
@@ -7,9 +7,9 @@
 #SBATCH --error=bwjellyhisto.err # error file
 #SBATCH --ntasks=1 # Run 1 job
 #SBATCH --ntasks-per-node=1 # one task per computer
-#SBATCH --cpus-per-task=8 # 8 CPUS per job
+#SBATCH --cpus-per-task=8 # 2 CPUS per job
 #SBATCH --mem=4GB # memory limit of 4 GB
 
 module load jellyfish
 
-jellyfish histo -t 8 reads.jf > reads.histo
+jellyfish histo output > output.histo
