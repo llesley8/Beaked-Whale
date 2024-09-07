0b2764c28b331e63cf9ee696fbec9e75ee3d9f95
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-06 19:19:25 -0700

    Update and rename Jellyfish script 2 to Jellyfish script 2 - bwjellyfish2.sh

--- Jellyfish script 2 - bwjellyfish2.sh
+++ Jellyfish script 2 - bwjellyfish2.sh
@@ -11,3 +11,5 @@
 #SBATCH --mem=60GB # memory limit of 60 GB
 
 jellyfish count -C -m 21 -s 10000000 -c 3 -t 12 /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpress/m84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz -o reads.jf
+
+#use this script
