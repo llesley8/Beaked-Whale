24e3c10dd165feacc818caa149390b3bc0188f33
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-09 10:53:53 -0700

    Update and rename bwkmc4_histo.sh to bwhisto_v1.sh

--- bwhisto_v1.sh
+++ bwhisto_v1.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=bwkmc4_histo # Name the job bwfastqc to the queue
+#SBATCH --job-name=bwhisto # Name the job bwfastqc to the queue
 #SBATCH --time=0-02:00:00 # Wall clock time limit in Days-Hours:min:seconds
 #SBATCH --mail-user=llugardo@ucsc.edu # send updates to my email
 #SBATCH --mail-type=ALL # send all types of updates
-#SBATCH --output=bwkmc4_histo.out # output file
-#SBATCH --error=bwkmc4_histo.err # error file
+#SBATCH --output=bwhisto.out # output file
+#SBATCH --error=bwhisto.err # error file
 #SBATCH --ntasks=1 # Run 1 job
 #SBATCH --ntasks-per-node=1 # one task per computer
 #SBATCH --cpus-per-task=2 # 2 CPUS per job
-#SBATCH --mem=30GB # memory limit of 30 GB
+#SBATCH --mem=4GB # memory limit of 4 GB
 
-kmc_tools transform 21mers histogram 21mers.histo -cx100000000
+module load miniconda3
+conda activate /hb/home/llugardo/envs/kmc
+kmc_tools transform 21mers_v1 histogram 21mers_v1.histo
