30b557cff92e54dfb0ac79cf38a642faf5fc4f10
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-09 13:33:37 -0700

    Update Nanoplot - bwnanoplot.sh

--- Nanoplot - bwnanoplot.sh
+++ Nanoplot - bwnanoplot.sh
@@ -7,14 +7,13 @@
 #SBATCH --error=bwnanoplot.err # error file
 #SBATCH --ntasks=1 # Run 1 job
 #SBATCH --ntasks-per-node=1 # one task per computer
-#SBATCH --cpus-per-task=14 # 14 CPUS per job
+#SBATCH --cpus-per-task=16 # 16 CPUS per job
 #SBATCH --mem=64GB # memory limit of 64 GB
 
 module load miniconda3
-conda activate /hb/home/llugardo/envs/nanoplot
+conda activate /hb/home/llugardo/envs/NanoPlot
 
-NanoPlot -t 14 --fastq /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpress/m84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz --plots kde dot --legacy hex --N50 --no_static --only-report
+NanoPlot -t 16 --fastq /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpress/m84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz --plots kde dot --legacy hex --N50 --no_static --only-report
 
 #located in "nanoplot" directory
 #to get the hex output rerun with --legacy hex
-#included --no_static and --only-report to help prevent crash with NanoPlot v 1.43.0 --> did not work
