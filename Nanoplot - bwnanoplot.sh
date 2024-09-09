024efb033d09a23ec8cfa8676a9f6a84832736da
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-09 02:59:17 -0700

    Update Nanoplot - bwnanoplot.sh

--- Nanoplot - bwnanoplot.sh
+++ Nanoplot - bwnanoplot.sh
@@ -8,12 +8,12 @@
 #SBATCH --ntasks=1 # Run 1 job
 #SBATCH --ntasks-per-node=1 # one task per computer
 #SBATCH --cpus-per-task=14 # 14 CPUS per job
-#SBATCH --mem=70GB # memory limit of 70 GB
+#SBATCH --mem=64GB # memory limit of 64 GB
 
 module load miniconda3
 conda activate /hb/home/llugardo/envs/nanoplot
 
-NanoPlot -t 14 --fastq /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpress/m84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz --plots kde dot --legacy hex --N50
+NanoPlot -t 14 --fastq /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpress/m84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz --plots kde dot --legacy hex --N50 --no_static
 
 #located in "nanoplot" directory
 #WARNING: hex as part of --plots has been deprecated and will be ignored. To get the hex output, rerun with --legacy hex.
