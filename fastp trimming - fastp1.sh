71b495518243c6c00848e39b28f9a4950daeacb3
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-11 19:07:29 -0700

    Update fastp trimming - fastp1.sh

--- fastp trimming - fastp1.sh
+++ fastp trimming - fastp1.sh
@@ -7,7 +7,7 @@
 #SBATCH --error=fastp.err # error file
 #SBATCH --ntasks=1 # Run 1 job
 #SBATCH --ntasks-per-node=1 # one task per computer
-#SBATCH --cpus-per-task=2 # 2 CPUS per job
+#SBATCH --cpus-per-task=8 # 8 CPUS per job
 #SBATCH --mem=40GB # memory limit of 40 GB
 
 module load miniconda3
