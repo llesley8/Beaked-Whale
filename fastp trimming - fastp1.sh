2d7a86d207960b4bb618d633fd1fe391f4acf354
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-11 19:06:56 -0700

    Update fastp trimming - fastp1.sh

--- fastp trimming - fastp1.sh
+++ fastp trimming - fastp1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=fastp # Name the job bwjellyfish to the queue
+#SBATCH --job-name=fastp # Name the job fastp to the queue
 #SBATCH --time=0-12:00:00 # Wall clock time limit in Days-Hours:min:seconds
 #SBATCH --mail-user=llugardo@ucsc.edu # send updates to my email
 #SBATCH --mail-type=ALL # send all types of updates
