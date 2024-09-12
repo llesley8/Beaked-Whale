9aaa68f2440aea8fe5384fc6ab7fe4acb9f349d6
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-11 19:41:20 -0700

    Update fastp trimming - fastp1.sh

--- fastp trimming - fastp1.sh
+++ fastp trimming - fastp1.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1 # Run 1 job
 #SBATCH --ntasks-per-node=1 # one task per computer
 #SBATCH --cpus-per-task=8 # 8 CPUS per job
-#SBATCH --mem=40GB # memory limit of 40 GB
+#SBATCH --mem=64GB # memory limit of 40 GB
 
 module load miniconda3
 conda activate /hb/home/llugardo/envs/fastp
