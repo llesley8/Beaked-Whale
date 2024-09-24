b17eed16098ef0b6587a2e707be04375f57856b7
Lesley Lugardo <llugardo@ucsc.edu>
2024-09-24 12:00:14 -0700

    Create Hifiasm primary assembly - hifiprimary.sh

---
+++ Hifiasm primary assembly - hifiprimary.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=hifiprimary # Name the job hifiprimary to the queue
+#SBATCH --time=2-12:00:00 # Wall clock time limit in Days-Hours:min:seconds
+#SBATCH --mail-user=llugardo@ucsc.edu # send updates to my email
+#SBATCH --mail-type=ALL # send all types of updates
+#SBATCH --output=hifiprimary.out # output file
+#SBATCH --error=hifiprimary.err # error file
+#SBATCH --ntasks=1 # Run 1 job
+#SBATCH --ntasks-per-node=1 # one task per computer
+#SBATCH --cpus-per-task=12 # 12 CPUS per job
+#SBATCH --mem=80GB # memory limit of 80 GB
+
+module load miniconda3
+conda activate /hb/groups/kelley_training/lesley/envs/hifiasm
+hifiasm -o Hifiprimary.asm --primary -t 12 /hb/groups/kelley_training/beaked_whale/PBmixRevio1579_2_A01_PHGP_30hours_1-121kbExpressCCSv32hrPE_250pM_Mesoplodon_bc2072_CCSExpress/m84053_240607_205216_s1.hifi_reads.bc2072.fastq.gz
